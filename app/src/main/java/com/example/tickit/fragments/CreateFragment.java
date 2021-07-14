121224f83859fdf1f3e6088c90a458613adefe98
mahakbindal <mahakbindal@gmail.com>
2021-07-14 08:38:59 -0700

    Implemented Google Maps Directions API to display route between two locations

--- app/src/main/java/com/example/tickit/fragments/CreateFragment.java
+++ app/src/main/java/com/example/tickit/fragments/CreateFragment.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,9 +33,16 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.internal.PolylineEncoding;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -50,6 +60,7 @@ public class CreateFragment extends Fragment {
     GoogleMap mGoogleMap;
     public List<List<Address>> mLocations;
     public List<LatLng> mLatLngList;
+    private GeoApiContext mGeoApiContext = null;
 
     public CreateFragment() {
         // Required empty public constructor
@@ -76,6 +87,10 @@ public class CreateFragment extends Fragment {
 
             }
         });
+
+        if(mGeoApiContext == null) {
+            mGeoApiContext = new GeoApiContext.Builder().apiKey(getString(R.string.google_maps_api_key)).build();
+        }
         return mBinding.getRoot();
     }
 
@@ -95,7 +110,6 @@ public class CreateFragment extends Fragment {
 
     private void geoLocate(View v) {
         ArrayList<Integer> locationId = new ArrayList<>(Arrays.asList(R.id.etLocation1, R.id.etLocation2));
-
         Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
         try {
             for(int id: locationId) {
@@ -118,8 +132,14 @@ public class CreateFragment extends Fragment {
                 LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
                 mLatLngList.add(latLng);
                 Log.i(TAG, "latlng points: " + mLatLngList);
+                Marker marker = mGoogleMap.addMarker(new MarkerOptions().position(latLng));
+                if(mLocations.indexOf(location) == 1){
+                    Log.i(TAG, "Current location: " + location);
+                    calculateDirections(marker);
+                }
+
                 mGoogleMap.addMarker(new MarkerOptions().position(latLng));
-                drawPolyline();
+//                drawPolyline();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -143,5 +163,73 @@ public class CreateFragment extends Fragment {
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 4);
         mGoogleMap.moveCamera(cameraUpdate);
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    private void calculateDirections(Marker marker){
+        Log.d(TAG, "calculateDirections: calculating directions.");
+        List<com.google.maps.model.LatLng> convertedLatLngList = convertCoordType(mLatLngList);
+
+        com.google.maps.model.LatLng destination = convertedLatLngList.get(convertedLatLngList.size()-1);
+        LatLng dst = mLatLngList.get(1);
+        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
+
+        directions.alternatives(false);
+        directions.origin(convertedLatLngList.get(0));
+        Log.i(TAG, "Origin location: " + mLatLngList.get(0));
+        Log.d(TAG, "calculateDirections: destination: " + destination.toString());
+        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                Log.d(TAG, "onResult: routes: " + result.routes[0].toString());
+                Log.d(TAG, "onResult: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
+                addPolylinesToMap(result);
+            }
+
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e(TAG, "onFailure: ", e);
+
+            }
+        });
+    }
+
+    private void addPolylinesToMap(final DirectionsResult result){
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "run: result routes: " + result.routes.length);
+
+                for(DirectionsRoute route: result.routes){
+                    Log.d(TAG, "run: leg: " + route.legs[0].toString());
+                    List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(route.overviewPolyline.getEncodedPath());
+
+                    List<LatLng> newDecodedPath = new ArrayList<>();
+
+                    // This loops through all the LatLng coordinates of ONE polyline.
+                    for(com.google.maps.model.LatLng latLng: decodedPath){
+
+//                        Log.d(TAG, "run: latlng: " + latLng.toString());
+
+                        newDecodedPath.add(new LatLng(
+                                latLng.lat,
+                                latLng.lng
+                        ));
+                    }
+                    Polyline polyline = mGoogleMap.addPolyline(new PolylineOptions().addAll(newDecodedPath));
+                    polyline.setColor(ContextCompat.getColor(getActivity(), R.color.olive));
+                    polyline.setClickable(true);
+
+                }
+            }
+        });
+    }
+
+    // https://stackoverflow.com/questions/60245464/different-latlng-imports
+    static List<com.google.maps.model.LatLng> convertCoordType(List<com.google.android.gms.maps.model.LatLng> list) {
+        List<com.google.maps.model.LatLng> resultList = new ArrayList<>();
+        for (com.google.android.gms.maps.model.LatLng item : list) {
+            resultList.add(new com.google.maps.model.LatLng(item.latitude, item.longitude));
+        }
+        return resultList;
     }
 }
